048672e6b02b843f2a221450d38533199fb5cae9
NitinPraksash9911 <nitin.prakash9911@gmail.com>
2022-01-11 14:53:31 +0530

    remove unused string from HttErrors

--- app/src/main/java/com/example/catcraft/arch/Resource.kt
+++ app/src/main/java/com/example/catcraft/arch/Resource.kt
@@ -6,13 +6,13 @@ sealed class Resource<out T> {
     data class NetworkException(val error: String) : Resource<Nothing>()
     data class Error(val error: String) : Resource<Nothing>()
 
-    sealed class HttpErrors(val msg: String) : Resource<Nothing>() {
-        data class ResourceForbidden(val exception: String) : HttpErrors(exception)
-        data class ResourceNotFound(val exception: String) : HttpErrors(exception)
-        data class InternalServerError(val exception: String) : HttpErrors(exception)
-        data class BadGateWay(val exception: String) : HttpErrors(exception)
-        data class ResourceRemoved(val exception: String) : HttpErrors(exception)
-        data class RemovedResourceFound(val exception: String) : HttpErrors(exception)
+    sealed class HttpErrors : Resource<Nothing>() {
+        data class ResourceForbidden(val exception: String) : HttpErrors()
+        data class ResourceNotFound(val exception: String) : HttpErrors()
+        data class InternalServerError(val exception: String) : HttpErrors()
+        data class BadGateWay(val exception: String) : HttpErrors()
+        data class ResourceRemoved(val exception: String) : HttpErrors()
+        data class RemovedResourceFound(val exception: String) : HttpErrors()
     }
 
 }
